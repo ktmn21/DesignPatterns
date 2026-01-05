7087cd71adce3885901c57cdc2092b4b33cffed3
kutman_mukarapov <kutman2002mukarapov@gmail.com>
2026-01-05 20:15:41 +0600

    completed the chellage 'notes'

---
+++ StructuralDesignPattern/Composite Patterns/ChellangeNotes/src/Note.java
@@ -0,0 +1,15 @@
+
+public class Note implements Playable{
+
+    private final char value;
+
+    public Note(char value) {
+        this.value = value;
+    }
+
+    @Override
+    public void play() {
+        System.out.println(value);
+    }
+
+}
